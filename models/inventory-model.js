2841d5739788ef271b52b80b13995c14b520d71d
Ross L. Owen <rowen@byupathway.edu>
2025-05-19 18:36:22 -0600

    week3 nav query

---
+++ models/inventory-model.js
@@ -0,0 +1,10 @@
+﻿const pool = require("../database/")
+
+/* ***************************
+ *  Get all classification data
+ * ************************** */
+async function getClassifications(){
+    return await pool.query("SELECT * FROM public.classification ORDER BY classification_name")
+}
+
+module.exports = {getClassifications}
