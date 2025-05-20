bfa432718b78042b491a9eb3df22f3435c2a82a5
Ross L. Owen <rowen@byupathway.edu>
2025-05-19 19:54:30 -0600

    week3: classification page. error handling

--- server.js
+++ server.js
@@ -11,6 +11,8 @@ const env = require('dotenv').config()
 const app = express()
 const staticFiles = require('./routes/static')
 const baseController = require("./controllers/baseController")
+const inventoryRoute = require("./routes/inventoryRoute")
+const utilities = require("./utilities/")
 
 /* ***********************
  * View Engine and Templates
@@ -24,7 +26,35 @@ app.set('layout', './layouts/layout') // not at views root
  *************************/
 app.use(staticFiles)
 // Index route
-app.get('/', baseController.buildHome)
+app.get('/', utilities.handleErrors(baseController.buildHome))
+// Inventory routes
+app.use("/inv", utilities.handleErrors(inventoryRoute))
+
+
+// File Not Found Route - must be last route in list
+app.use(async (req, res, next) => {
+  next({status: 404, message: 'Sorry, we appear to have lost that page.'})
+})
+
+
+/* ***********************
+* Express Error Handler
+* Place after all other middleware
+*************************/
+app.use(async (err, req, res, next) => {
+  let nav = await utilities.getNav()
+  console.error(`Error at: "${req.originalUrl}": ${err.message}`)
+  if (err.status === 404) {
+    message = err.message
+  } else {
+    message = 'Something went wrong! Please try again. If the problem persists, contact me on the corner, near the flagpole, for your beating.'
+  }
+  res.render("errors/error", {
+    title: err.status || 'Server Error',
+    message: message,
+    nav
+  })
+})
 
 /* ***********************
  * Local Server Information
