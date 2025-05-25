e9d2fab31adeafa2e8c622574a934117708c6ef9
Ross L. Owen <rowen@byupathway.edu>
2025-05-24 18:20:34 -0600

    week4: login page

---
+++ controllers/accountController.js
@@ -0,0 +1,11 @@
+﻿const utilities = require('../utilities')
+
+async function buildLogin(req, res, next) {
+    let nav = await utilities.getNav()
+    res.render('account/login', {
+        title: 'Login',
+        nav,
+    })
+}
+
+module.exports = {buildLogin}
--- server.js
+++ server.js
@@ -13,6 +13,29 @@ const staticFiles = require('./routes/static')
 const baseController = require("./controllers/baseController")
 const inventoryRoute = require("./routes/inventoryRoute")
 const utilities = require("./utilities/")
+const session = require("express-session")
+const pool = require('./database/')
+
+/* ***********************
+ * Middleware
+ * ************************/
+app.use(session({
+  store: new (require('connect-pg-simple')(session))({
+    createTableIfMissing: true,
+    pool,
+  }),
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+  name: 'sessionId',
+}))
+
+// Express Messages Middleware
+app.use(require('connect-flash')())
+app.use(function(req, res, next){
+  res.locals.messages = require('express-messages')(req, res)
+  next()
+})
 
 /* ***********************
  * View Engine and Templates
@@ -29,13 +52,13 @@ app.use(staticFiles)
 app.get('/', utilities.handleErrors(baseController.buildHome))
 // Inventory routes
 app.use("/inv", utilities.handleErrors(inventoryRoute))
-
+// Account routes
+app.use("/account", require("./routes/accountRoute"))
 
 // File Not Found Route - must be last route in list
 app.use(async (req, res, next) => {
   next({status: 404, message: 'Sorry, we appear to have lost that page.'})
 })
-
 
 /* ***********************
 * Express Error Handler
