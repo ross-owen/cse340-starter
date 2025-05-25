c57d6ffa6d1a6431f58279e54b2c802a743631e9
Ross L. Owen <rowen@byupathway.edu>
2025-05-24 20:57:02 -0600

    week4: server side validation

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -2,9 +2,15 @@
 const router = new express.Router()
 const accountController = require('../controllers/accountController')
 const utilities = require('../utilities')
+const validator = require('../utilities/account-validation')
 
 router.get('/login', utilities.handleErrors(accountController.buildLogin))
 router.get('/register', utilities.handleErrors(accountController.buildRegister))
-router.post('/register', utilities.handleErrors(accountController.register))
+router.post(
+    '/register',
+    validator.registrationRules(),
+    validator.checkData,
+    utilities.handleErrors(accountController.register)
+)
 
 module.exports = router
---
+++ utilities/account-validation.js
@@ -0,0 +1,61 @@
+﻿const utilities = require('.')
+const {body, validationResult} = require('express-validator')
+const validator = {}
+
+validator.registrationRules = () => {
+    return [
+        body('firstName')
+            .trim()
+            .isString()
+            .isLength({min: 1})
+            .withMessage('Please provide a first name'),
+        body('lastName')
+            .trim()
+            .isString()
+            .isLength({min: 1})
+            .withMessage('Please provide a last name'),
+        body('email')
+            .trim()
+            .isEmail()
+            .normalizeEmail()
+            .withMessage('Please provide a valid email address'),
+        body('password')
+            .trim()
+            .isStrongPassword({
+                minLength: 12,
+                minLowercase: 1,
+                minUppercase: 1,
+                minNumber: 1,
+                minSymbols: 1
+            })
+            .normalizeEmail()
+            .withMessage('Please provide a valid password that meets the specified requirements'),
+    ]
+}
+
+validator.checkData = async (req, res, next) => {
+    const {
+        firstName,
+        lastName,
+        email,
+    } = req.body
+
+    let errors = []
+    errors = validationResult(req)
+
+    if (!errors.isEmpty()) {
+        let nav = await utilities.getNav()
+        res.render('account/register', {
+            errors,
+            title: 'Register',
+            nav,
+            firstName,
+            lastName,
+            email,
+        })
+        return
+    }
+    next()
+}
+
+module.exports = validator
